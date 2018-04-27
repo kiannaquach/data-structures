b9b99ee49367f199c9471b5e93593296f63b7cf5
kiannaquach <kiannaquach.kq@gmail.com>
2018-04-26 17:45:05 -0700

    finished up to graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,30 +2,67 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = [];
+  this.edges = [];
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function(value) {
+  var node = {};
+  node.value = value;
+  this.nodes.push(node.value);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function(value) {
+  for (var i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i] === value) {
+      return true;
+    }
+  }
+ 
+  return false; 
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function(value) {
+  var result;
+  
+  for (var i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i] === value) {
+      result = this.nodes.splice(i, 1)
+    }
+  }
+
+  return result;
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for (var i = 0; i < this.edges.length; i++) {
+    if (this.edges[i][0] === fromNode && this.edges[i][1] === toNode) {
+      return true;
+    }  
+  }
+  
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges.push([fromNode, toNode])
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var result;
+
+    for (var i = 0; i < this.edges.length; i++) {
+      if (this.edges[i][0] === fromNode && this.edges[i][1] === toNode) {
+        this.edges[i].splice(i, 1)
+      }
+    }
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,15 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value) 
+    
+
+    if (!list.head) { //  equal to null
+      list.head = node; // run this the first time
+      // list.tail = node;
+    } else if (list.tail) { // if list.tail is not the last one
+      
+      list.tail.next = node;  // it keep moving to next node until reaching null                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            t = node;
+    }
+
+    list.tail = node;  //reached the end add node
   };
 
   list.removeHead = function() {
+    var firstHead = list.head;
+    list.head = firstHead.next;
+    return firstHead.value;
   };
 
   list.contains = function(target) {
+    var currentHead = list.head;
+    
+    while (currentHead) {
+      if (currentHead.value === target) {
+         return true;
+      } 
+       
+      currentHead = currentHead.next; // go to next node
+    }
+    return false;
+ 
   };
-
   return list;
+
 };
 
 var Node = function(value) {
