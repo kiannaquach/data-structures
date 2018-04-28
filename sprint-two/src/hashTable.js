115380d84557e3a244db0ca5bd238c7de7134ea5
kiannaquach <kiannaquach.kq@gmail.com>
2018-04-28 10:49:07 -0700

    finished sprint-two

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,25 +8,76 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   
-  var bucket = this._storage.set(index);
+  var bucket = this._storage.get(index);
 
   if (!bucket) {
     bucket = [];
-    this._storage[index] = bucket;
+    this._storage.set(index, bucket);
   }
 
-  bucket.push([k,v])
+
+  // isFound will be updated
+  var updateTuple = false;
+
+  // iterate through bucket
+  for (var i = 0; i < bucket.length; i++) {
+    // look at that bucket's tuple
+    // check to see bucket's tuple is same as key provided
+    if (bucket[i][0] === k) {
+      debugger;
+      // set that tuple at index 1 to the value
+      bucket[i][1] = v;
+      // update updateTuple 
+      updateTuple = true;
+    }
+  }
   
+  // if that k,v is found
+  if (!updateTuple) {
+    // push the tuple into bucket
+    bucket.push([k, v]); 
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index);
+  
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index);
+  var value;
+  
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      value = bucket[i][1];
+      bucket[i].splice(1, 1);
+      return value;
+    }
+  }
 };
 
+// var hashTable = new HashTable();
+
+// hashTable.insert('Steven', 'Seagal');
+// console.log(hashTable.retrieve('Steven'))
+
+// var v1 = 'val1';
+// var v2 = 'val2';
+// hashTable.insert(v1, v1);
+// hashTable.insert(v2, v2);
+// hashTable.retrieve(v1)
+// hashTable.retrieve(v2)
 
 
 /*
